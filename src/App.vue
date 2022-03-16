d05f17a936cc9b79acf8025d4adc35f77bf870f3
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-16 19:33:47 +0800

    feat: init plum

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,94 @@
 <script setup lang="ts">
+const el = $ref<HTMLCanvasElement>()
+const ctx = $computed(() => el!.getContext('2d')!)
 
+const WIDTH = 600
+const HEIGHT = 600
+
+interface Point {
+  x: number
+  y: number
+}
+
+interface Branch {
+  start: Point
+  length: number
+  theta: number
+}
+
+function init() {
+  ctx.strokeStyle = '#fff'
+
+  step({
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 20,
+    theta: -Math.PI / 2,
+  })
+}
+
+const pendingTasks: Function[] = []
+
+function step(b: Branch, depth = 0) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+
+  if (depth < 2 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta - 0.3 * Math.random(),
+    }, depth + 1))
+  }
+  if (depth < 2 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta + 0.3 * Math.random(),
+    }, depth + 1))
+  }
+}
+
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let framesCount = 0
+function startFrame() {
+  requestAnimationFrame(() => {
+    framesCount += 1
+    if (framesCount % 3 === 0)
+      frame()
+    startFrame()
+  })
+}
+
+startFrame()
+
+function lineTo(p1: Point, p2: Point) {
+  ctx.beginPath()
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
+  ctx.stroke()
+}
+
+function getEndPoint(b: Branch): Point {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
+  }
+}
+
+function drawBranch(b: Branch) {
+  lineTo(b.start, getEndPoint(b))
+}
+
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
-  <div>
-    Hi
-  </div>
+  <canvas ref="el" width="600" height="600" border />
 </template>>
