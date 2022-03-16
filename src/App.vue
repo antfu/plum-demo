4d7e4cb83ec449b071e9d4a8232ba2353b504d79
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-16 19:07:00 +0800

    chore: clean up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
-    <Footer />
-  </main>
-</template>
+  <div>
+    Hi
+  </div>
+</template>>
